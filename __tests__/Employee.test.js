fb0e2e1ce89dcd60700a709dd3b30c14247e6cf9
Tabatha Bell <tabathambell@live.com>
2021-01-07 18:25:28 -0500

    running tests installed npms

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee.js');
+
+test('creates new slot', () => {
+    const employee = new Employee();
+    expect(typeof (employee)).toBe('slot');
+});
